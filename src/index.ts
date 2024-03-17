c57bad76ce1af5a475a2913fdc14e06345e95de2
Vuko Vukasinovic <vukasinovicvuko5@gmail.com>
2024-03-17 10:41:31 +0100

    store data in json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { scrapeEtsy } from './controllers/controller';
+import fs from 'fs/promises';
 
 async function startScraping() {
   try {
     const products = await scrapeEtsy();
+    const jsonData = JSON.stringify(products, null, 2);
+    await fs.writeFile('scrapedProducts.json', jsonData, 'utf-8');
     console.log('Scraped Products:', products);
   } catch (error) {
     console.error('Error during scraping:', error);
